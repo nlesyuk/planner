90c93d23ec3767bc5876f18cb531c1b3e48eabe5
Nazar Lesyuk <nazariy.lesyuk@gmail.com>
2023-07-11 19:13:20 +0300

    webpack config for styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const mode = process.env.NODE_ENV
 const devMode = mode === 'development'
@@ -13,6 +15,38 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'), // folder
     clean: true, // clear before build
-    filename: 'index.[contenthash].js',
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader': MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')]
+              }
+            }
+          },
+          "sass-loader",
+        ],
+      },
+    ]
   }
 }
